c0cc3a3a4f6da2ae69f9486a005d1afa1cf4b6ba
Denis Velasquez <vgdenisomar@gmail.com>
2019-06-03 22:34:43 -0600

    put and delete

--- obtBackEnd/routes/api/things.js
+++ obtBackEnd/routes/api/things.js
@@ -1,8 +1,69 @@
 const express=require('express');
 var router = express.Router();
 
+var thingsCollection=[];
+
+var thingsStruct={
+    "id":0,
+    "descripcion":'',
+    "fecha":0,
+    "by":''
+
+};
+
+thingsCollection.push(
+    Object.assign(
+        thingsStruct,
+        {
+            "id":"1",
+            "descripcion":"Mi primer thing",
+            "fecha":'12',
+            "by":'Denis'
+
+        }
+    )
+    
+);
+
 router.get('/',(req,res,next)=>{
-    res.status(200).json({"moudule":"Things"})
+    res.status(200).json(thingsCollection)
+});
+
+router.post('/',(req,res,next)=>{
+    var newElement=Object.assign({},thingsStruct, req.body);
+    thingsCollection.push(newElement);
+    res.status(200).json(newElement)
+});
+
+
+router.put('/:id',(req,res,next)=>{
+    const requestId=req.params.id;
+    
+    let thingsStruct=thingsCollection.filter(thingsStruct=>{
+        return thingsStruct.id==requestId;
+    })[0];
+
+    const index=thingsCollection.indexOf(thingsStruct);
+    const keys=Object.keys(req.body);
+    keys.forEach(key=>{
+        thingsStruct[key]=req.body[key];
+    })
+    thingsCollection[index]=thingsStruct;
+    res.json(thingsCollection[index]);
+});
+
+
+router.delete('/:id',(req,res,next)=>{
+    const requestId=req.params.id;
+    
+    let thingsStruct=thingsCollection.filter(thingsStruct=>{
+        return thingsStruct.id==requestId;
+    })[0];
+
+    const index=thingsCollection.indexOf(thingsStruct);
+
+    thingsCollection.splice(index,1);
+    res.json({message:'El bigthing eliminado'});
 });
 
 
